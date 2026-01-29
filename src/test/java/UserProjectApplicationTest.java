28c8c61b67ceff6d1e8d2fc2188e570d8a74e644
Lucas Teixeira <lucast.leopoldino.rodrigues@gmail.com>
2026-01-29 20:23:23 -0300

    Arrumando os testes unitarios

--- src/test/java/UserProjectApplicationTest.java
+++ src/test/java/UserProjectApplicationTest.java
@@ -1,7 +1,10 @@
+import com.lucasteixeira.bank.UserProjectApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = UserProjectApplication.class)
+@ActiveProfiles("test")
 public class UserProjectApplicationTest {
 
     @Test
